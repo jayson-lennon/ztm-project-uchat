2feaa979296738c9d516b8aadb7c99f5d22780a1
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-03 14:16:48 -0700

    fe-register

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,11 +1,14 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
+use dioxus_router::Router;
 use fermi::use_init_atom_root;
 
 pub fn App(cx: Scope) -> Element {
     use_init_atom_root(cx);
     cx.render(rsx! {
-        h1 { "hello, world!" }
+        Router {
+            Route { to: "some_url", page::Register {} },
+        }
     })
 }
---
+++ frontend/src/page/register.rs
@@ -0,0 +1,7 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+
+pub fn Register(cx: Scope) -> Element {
+    cx.render(rsx! {""})
+}
