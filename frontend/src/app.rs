2167edffd00f269652e515ac693ab777c189267a
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-03 14:19:57 -0700

    fe-register-add-route

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,14 +1,16 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
-use dioxus_router::Router;
+use dioxus_router::{Route, Router};
 use fermi::use_init_atom_root;
+
+pub use crate::prelude::*;
 
 pub fn App(cx: Scope) -> Element {
     use_init_atom_root(cx);
     cx.render(rsx! {
         Router {
-            Route { to: "some_url", page::Register {} },
+            Route { to: page::ACCOUNT_REGISTER, page::Register {} },
         }
     })
 }
--- frontend/src/page/register.rs
+++ frontend/src/page/register.rs
@@ -3,5 +3,5 @@
 use dioxus::prelude::*;
 
 pub fn Register(cx: Scope) -> Element {
-    cx.render(rsx! {""})
+    cx.render(rsx! {"Registration page"})
 }
