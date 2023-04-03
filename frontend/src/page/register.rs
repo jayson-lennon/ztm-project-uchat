de7b5bd8dec3222c5aa391fd085526d2cca595cb
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-03 14:34:39 -0700

    fe-register-form

--- frontend/src/page/register.rs
+++ frontend/src/page/register.rs
@@ -3,5 +3,17 @@
 use dioxus::prelude::*;
 
 pub fn Register(cx: Scope) -> Element {
-    cx.render(rsx! {"Registration page"})
+    cx.render(rsx! {
+        form {
+            class: "flex flex-col gap-5",
+            prevent_default: "onsubmit",
+            onsubmit: move |_| {},
+
+            button {
+                class: "btn",
+                r#type: "submit",
+                "Signup"
+            }
+        }
+    })
 }
