25ddb837108f2157de3180d224ce9c499768cd16
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-03 17:15:47 -0700

    fe-username-input

--- frontend/src/page/register.rs
+++ frontend/src/page/register.rs
@@ -16,6 +16,30 @@ impl PageState {
     }
 }
 
+#[inline_props]
+pub fn UsernameInput<'a>(
+    cx: Scope<'a>,
+    state: UseState<String>,
+    oninput: EventHandler<'a, FormEvent>,
+) -> Element<'a> {
+    cx.render(rsx! {
+        div {
+            class: "flex flex-col",
+            label {
+                r#for: "username",
+                "Username",
+            },
+            input {
+                id: "username",
+                name: "username",
+                class: "input-field",
+                placeholder: "User Name",
+                oninput: move |ev| oninput.call(ev),
+            }
+        }
+    })
+}
+
 pub fn Register(cx: Scope) -> Element {
     let page_state = PageState::new(cx);
     let page_state = use_ref(cx, || page_state);
