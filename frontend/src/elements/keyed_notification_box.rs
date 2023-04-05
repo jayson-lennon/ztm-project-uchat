ca1d738dc918eb6fcbc602bf854c99ec871099de
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-04 19:18:17 -0700

    fe-keyed-notif-box

--- frontend/src/elements/keyed_notification_box.rs
+++ frontend/src/elements/keyed_notification_box.rs
@@ -31,3 +31,34 @@ impl KeyedNotifications {
         !self.inner.is_empty()
     }
 }
+
+#[derive(PartialEq, Props)]
+pub struct KeyedNotificationsProps<'a> {
+    legend: Option<&'a str>,
+    notifications: KeyedNotifications,
+}
+
+pub fn KeyedNotificationBox<'a>(cx: Scope<'a, KeyedNotificationsProps<'a>>) -> Element {
+    let notifications = cx.props.notifications.messages().map(|msg| {
+        rsx! { li { "{msg}" } }
+    });
+
+    let legend = cx.props.legend.unwrap_or("Errors");
+
+    match cx.props.notifications.has_messages() {
+        true => cx.render(rsx! {
+            fieldset {
+                class: "fieldset border-red-300 rounded",
+                legend {
+                    class: "bg-red-300 px-4",
+                    "{legend}"
+                }
+                ul {
+                    class: "list-disc ml-4",
+                    notifications
+                }
+            }
+        }),
+        false => None,
+    }
+}
