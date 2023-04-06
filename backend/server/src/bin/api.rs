d071adbedbd9667938475ceb3996f54d614291cf
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-05 20:46:16 -0700

    api-binary-run

--- backend/server/src/bin/api.rs
+++ backend/server/src/bin/api.rs
@@ -1,6 +1,7 @@
 use clap::{command, Parser, Subcommand};
 use color_eyre::{eyre::Context, Help, Result};
 use std::net::SocketAddr;
+use tracing::{debug, info};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
@@ -30,6 +31,33 @@ enum Command {
 }
 
 async fn run() -> Result<()> {
+    let use_dotenv = dotenvy::dotenv();
+
+    let args = Cli::parse();
+
+    uchat_server::logging::setup(args.verbosity);
+
+    if let Ok(path) = use_dotenv {
+        debug!(target: "uchat_server", dot_env_found = true, path = %path.to_string_lossy());
+    } else {
+        debug!(target: "uchat_server", dot_env_found = false);
+    }
+
+    if let Some(command) = args.command {
+        match command {
+            Command::GenKey => {
+                let mut rng = uchat_crypto::new_rng();
+                info!(target: "uchat_server", "generating private key...");
+                let (key, _) = uchat_server::cli::gen_keys(&mut rng)?;
+                let path = "private_key.base64";
+                std::fs::write(path, key.as_str())?;
+                info!(target: "uchat_server", path=path, "private key saved to disk");
+                info!(target: "uchat_server", "set API_PRIVATE_KEY environment variable with the content of the key in order to use it");
+                return Ok(());
+            }
+        }
+    }
+
     Ok(())
 }
 
