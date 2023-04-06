c1057d6d1f07b1bc1ce3fffd95d462b7289d0803
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-05 20:02:36 -0700

    api-server-state

--- backend/server/src/lib.rs
+++ backend/server/src/lib.rs
@@ -1,2 +1,38 @@
+use axum::extract::FromRef;
+use uchat_query::{AsyncConnection, AsyncConnectionPool, QueryError};
 
+#[derive(FromRef, Clone)]
+pub struct AppState {
+    pub db_pool: AsyncConnectionPool,
+    pub signing_keys: uchat_crypto::sign::Keys,
+    pub rng: rand::rngs::StdRng,
+}
 
+impl AppState {
+    pub async fn connect(&self) -> Result<AsyncConnection, QueryError> {
+        self.db_pool.get().await
+    }
+}
+
+pub mod cli {
+    use color_eyre::{eyre::Context, Help};
+    use rand::{CryptoRng, RngCore};
+    use uchat_crypto::sign::{encode_private_key, EncodedPrivateKey, Keys};
+
+    pub fn gen_keys<R>(rng: &mut R) -> color_eyre::Result<(EncodedPrivateKey, Keys)>
+    where
+        R: CryptoRng + RngCore,
+    {
+        let (private_key, keys) = Keys::generate(rng)?;
+        let private_key = encode_private_key(private_key)?;
+        Ok((private_key, keys))
+    }
+
+    pub fn load_keys() -> color_eyre::Result<Keys> {
+        let private_key = std::env::var("API_PRIVATE_KEY")
+            .wrap_err("failed to locate private API key")
+            .suggestion("set API_PRIVATE_KEY environment variable")?;
+
+        Ok(Keys::from_encoded(private_key)?)
+    }
+}
