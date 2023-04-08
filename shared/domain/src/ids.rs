21c92f982a8eb5d28391c60bcbd1a4e0746fb6f9
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-08 13:31:06 -0700

    db-ids

--- shared/domain/src/ids.rs
+++ shared/domain/src/ids.rs
@@ -1,45 +1,58 @@
-#[derive(
-    Clone, Copy, Debug, Eq, Hash, serde::Deserialize, serde::Serialize, PartialEq, Ord, PartialOrd,
-)]
-#[cfg_attr(feature = "query", derive(DieselNewType))]
-pub struct UserId(uuid::Uuid);
+macro_rules! new_id {
+    ($name:ident) => {
+        #[derive(
+            Clone,
+            Copy,
+            Debug,
+            Eq,
+            Hash,
+            serde::Deserialize,
+            serde::Serialize,
+            PartialEq,
+            Ord,
+            PartialOrd,
+        )]
+        #[cfg_attr(feature = "query", derive(DieselNewType))]
+        pub struct $name(uuid::Uuid);
 
-impl UserId {
-    pub fn new() -> Self {
-        Self(uuid::Uuid::new_v4())
-    }
+        impl $name {
+            pub fn new() -> Self {
+                Self(uuid::Uuid::new_v4())
+            }
 
-    pub fn into_inner(self) -> uuid::Uuid {
-        self.0
-    }
+            pub fn into_inner(self) -> uuid::Uuid {
+                self.0
+            }
 
-    pub fn as_uuid(&self) -> &uuid::Uuid {
-        &self.0
-    }
+            pub fn as_uuid(&self) -> &uuid::Uuid {
+                &self.0
+            }
 
-    pub fn to_string(&self) -> String {
-        self.0.to_string()
-    }
-}
-impl Default for UserId {
-    fn default() -> Self {
-        Self::new()
-    }
-}
+            pub fn to_string(&self) -> String {
+                self.0.to_string()
+            }
+        }
+        impl Default for $name {
+            fn default() -> Self {
+                Self::new()
+            }
+        }
 
-impl From<uuid::Uuid> for UserId {
-    fn from(id: uuid::Uuid) -> Self {
-        UserId(id)
-    }
-}
+        impl From<uuid::Uuid> for $name {
+            fn from(id: uuid::Uuid) -> Self {
+                $name(id)
+            }
+        }
 
-impl std::str::FromStr for UserId {
-    type Err = IdError;
-    fn from_str(id: &str) -> Result<Self, Self::Err> {
-        uuid::Uuid::try_parse(id)
-            .map(|id| id.into())
-            .map_err(|_| IdError::Parse)
-    }
+        impl std::str::FromStr for $name {
+            type Err = IdError;
+            fn from_str(id: &str) -> Result<Self, Self::Err> {
+                uuid::Uuid::try_parse(id)
+                    .map(|id| id.into())
+                    .map_err(|_| IdError::Parse)
+            }
+        }
+    };
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -47,3 +60,6 @@ pub enum IdError {
     #[error("failed to parse ID")]
     Parse,
 }
+
+new_id!(UserId);
+new_id!(SessionId);
