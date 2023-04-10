382aeea5f547d33d45de8b3958bfb0ea1633f700
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-10 09:32:27 -0700

    auth-handler

--- backend/server/src/handler.rs
+++ backend/server/src/handler.rs
@@ -1,7 +1,11 @@
 use axum::{async_trait, extract::State, response::IntoResponse, Json};
 use serde::Deserialize;
 
-use crate::{error::ApiResult, extractor::DbConnection, AppState};
+use crate::{
+    error::ApiResult,
+    extractor::{DbConnection, UserSession},
+    AppState,
+};
 
 pub mod user;
 
@@ -24,4 +28,27 @@ where
     Req: PublicApiRequest + Deserialize<'a>,
 {
     payload.process_request(conn, state).await
+}
+
+#[async_trait]
+pub trait AuthorizedApiRequest {
+    type Response: IntoResponse;
+    async fn process_request(
+        self,
+        conn: DbConnection,
+        session: UserSession,
+        state: AppState,
+    ) -> ApiResult<Self::Response>;
+}
+
+pub async fn with_handler<'a, Req>(
+    conn: DbConnection,
+    session: UserSession,
+    State(state): State<AppState>,
+    Json(payload): Json<Req>,
+) -> ApiResult<Req::Response>
+where
+    Req: AuthorizedApiRequest + Deserialize<'a>,
+{
+    payload.process_request(conn, session, state).await
 }
