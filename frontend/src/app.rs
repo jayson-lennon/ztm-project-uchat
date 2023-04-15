b5b63db47319dc69258767a75673383349358f3c
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-15 14:28:55 -0700

    fe-post-image-display

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -23,12 +23,19 @@ pub fn App(cx: Scope) -> Element {
 
     cx.render(rsx! {
         Router {
-            Route { to: page::ACCOUNT_REGISTER, page::Register {} },
-            Route { to: page::ACCOUNT_LOGIN, page::Login {} },
-            Route { to: page::HOME, page::Home {} },
-            Route { to: page::POST_NEW_CHAT, page::NewChat {} },
-            Route { to: page::POST_NEW_IMAGE, page::NewImage {} },
-            Route { to: page::POSTS_TRENDING, page::Trending {} },
+            main {
+                class: "max-w-[var(--content-max-width)]
+                min-w-[var(--content-min-width)]
+                mb-[var(--navbar-height)]
+                mx-auto
+                p-4",
+                Route { to: page::ACCOUNT_REGISTER, page::Register {} },
+                Route { to: page::ACCOUNT_LOGIN, page::Login {} },
+                Route { to: page::HOME, page::Home {} },
+                Route { to: page::POST_NEW_CHAT, page::NewChat {} },
+                Route { to: page::POST_NEW_IMAGE, page::NewImage {} },
+                Route { to: page::POSTS_TRENDING, page::Trending {} },
+            }
 
             ToastRoot { toaster: toaster },
             Navbar {}
--- frontend/src/elements/post/content.rs
+++ frontend/src/elements/post/content.rs
@@ -4,7 +4,9 @@ use crate::prelude::*;
 use dioxus::prelude::*;
 use uchat_domain::ids::PostId;
 
-use uchat_endpoint::post::types::{Chat as EndpointChat, PublicPost};
+use uchat_endpoint::post::types::{
+    Chat as EndpointChat, Image as EndpointImage, ImageKind, PublicPost,
+};
 
 #[inline_props]
 pub fn Chat<'a>(cx: Scope<'a>, post_id: PostId, content: &'a EndpointChat) -> Element {
@@ -26,12 +28,38 @@ pub fn Chat<'a>(cx: Scope<'a>, post_id: PostId, content: &'a EndpointChat) -> El
 }
 
 #[inline_props]
+pub fn Image<'a>(cx: Scope<'a>, post_id: PostId, content: &'a EndpointImage) -> Element {
+    let url = if let ImageKind::Url(url) = &content.kind {
+        url
+    } else {
+        return cx.render(rsx! { "image not found" });
+    };
+
+    let Caption = content
+        .caption
+        .as_ref()
+        .map(|caption| rsx! { figcaption { em { "{caption.as_ref()}" } } });
+
+    cx.render(rsx! {
+        figure {
+            class: "flex flex-col gap-2",
+            Caption,
+            img {
+                class: "w-full object-contain max-h-[80vh]",
+                src: "{url}"
+            }
+        }
+    })
+}
+
+#[inline_props]
 pub fn Content<'a>(cx: Scope<'a>, post: &'a PublicPost) -> Element {
     use uchat_endpoint::post::types::Content as EndpointContent;
     cx.render(rsx! {
         div {
             match &post.content {
                 EndpointContent::Chat(content) => rsx! { Chat { post_id: post.id, content: content} },
+                EndpointContent::Image(content) => rsx! { Image { post_id: post.id, content: content} },
                 _ => rsx! { "" }
             }
         }
