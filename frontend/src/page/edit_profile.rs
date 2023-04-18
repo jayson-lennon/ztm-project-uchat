4834bf84b5517758b586acc531bb27464b2aa00e
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-18 12:32:43 -0700

    pm-fe-img

--- frontend/src/page/edit_profile.rs
+++ frontend/src/page/edit_profile.rs
@@ -1,7 +1,8 @@
 #![allow(non_snake_case)]
 
-use crate::{elements::keyed_notification_box::KeyedNotifications, prelude::*};
+use crate::{elements::keyed_notification_box::KeyedNotifications, prelude::*, util};
 use dioxus::prelude::*;
+use web_sys::HtmlInputElement;
 
 #[derive(Clone, Debug)]
 enum PreviewImageData {
@@ -20,8 +21,96 @@ pub struct PageState {
     profile_image: Option<PreviewImageData>,
 }
 
+#[inline_props]
+pub fn ImageInput(cx: Scope, page_state: UseRef<PageState>) -> Element {
+    let toaster = use_toaster(cx);
+
+    cx.render(rsx! {
+        div {
+            label {
+                r#for: "image-input",
+                "Upload Image"
+            },
+            input {
+                class: "w-full",
+                id: "image-input",
+                r#type: "file",
+                accept: "image/*",
+                oninput: |_| {
+                    to_owned![page_state, toaster];
+                    async move {
+                        use gloo_file::{File, futures::read_as_data_url};
+                        use wasm_bindgen::JsCast;
+
+                        let el = util::document()
+                            .get_element_by_id("image-input")
+                            .unwrap()
+                            .unchecked_into::<HtmlInputElement>();
+                        let file: File = el.files().unwrap().get(0).unwrap().into();
+                        match read_as_data_url(&file).await {
+                            Ok(data) => page_state.with_mut(|state| state.profile_image = Some(PreviewImageData::DataUrl(data))),
+                            Err(e) => toaster.write().error(format!("Error loading file: {e}"), chrono::Duration::seconds(5)),
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+#[inline_props]
+pub fn ImagePreview(cx: Scope, page_state: UseRef<PageState>) -> Element {
+    let image_data = page_state.with(|state| state.profile_image.clone());
+
+    let img_el = |img_src| {
+        rsx! {
+            img {
+                class: "profile-portrait-lg",
+                src:"{img_src}",
+            }
+        }
+    };
+
+    let image_data = match image_data {
+        Some(PreviewImageData::DataUrl(ref data)) => img_el(data),
+        Some(PreviewImageData::Remote(ref url)) => img_el(url),
+        None => rsx! { div { "No image uploaded"}},
+    };
+
+    cx.render(rsx! {
+        div {
+            class: "flex flex-row justify-center",
+            image_data
+        }
+    })
+}
+
 pub fn EditProfile(cx: Scope) -> Element {
     let page_state = use_ref(cx, PageState::default);
+    let router = use_router(cx);
 
-    cx.render(rsx! {""})
+    cx.render(rsx! {
+        form {
+            class: "flex flex-col w-full gap-3",
+            prevent_default: "onsubmit",
+
+            ImagePreview { page_state: page_state.clone() },
+            ImageInput { page_state: page_state.clone() },
+
+            div {
+                class: "flex flex-row justify-end gap-3",
+                button {
+                    class: "btn",
+                    prevent_default: "onclick",
+                    onclick: move |_| router.pop_route(),
+                    "Cancel"
+                },
+                button {
+                    class: "btn",
+                    r#type: "submit",
+                    "Submit"
+                },
+            }
+        }
+    })
 }
