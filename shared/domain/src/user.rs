19a498bee65f6e1b34560cbd44b73de005dd76bb
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-18 12:49:51 -0700

    mp-fe-email

--- shared/domain/src/user.rs
+++ shared/domain/src/user.rs
@@ -1,4 +1,6 @@
 use nutype::nutype;
+use once_cell::sync::OnceCell;
+use regex::Regex;
 
 use crate::UserFacingError;
 
@@ -41,6 +43,43 @@ impl UserFacingError for DisplayNameError {
     fn formatted_error(&self) -> &'static str {
         match self {
             DisplayNameError::TooLong => "Display name is too long. Must be at most 30 characters.",
+        }
+    }
+}
+
+static EMAIL_REGEX: OnceCell<EmailRegex> = OnceCell::new();
+
+#[derive(Debug)]
+pub struct EmailRegex(Regex);
+
+impl EmailRegex {
+    pub fn global() -> &'static Self {
+        EMAIL_REGEX.get().expect("email regex is not initialized")
+    }
+
+    pub fn init() -> Self {
+        Self(regex::Regex::new(r#"^\S+@\S+\.\S{1,64}$"#).unwrap())
+    }
+
+    pub fn is_valid<T: AsRef<str>>(&self, text: T) -> bool {
+        self.0.is_match(text.as_ref())
+    }
+}
+
+fn is_valid_email(email: &str) -> bool {
+    let email_regex = EMAIL_REGEX.get_or_init(EmailRegex::init);
+
+    email_regex.is_valid(email)
+}
+
+#[nutype(validate(with = is_valid_email))]
+#[derive(AsRef, Clone, Debug, Serialize, Deserialize, PartialEq)]
+pub struct Email(String);
+
+impl UserFacingError for EmailError {
+    fn formatted_error(&self) -> &'static str {
+        match self {
+            EmailError::Invalid => "Email is not valid. Format: your_name@example.com",
         }
     }
 }
