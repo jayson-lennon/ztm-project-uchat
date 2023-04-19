746f21a96f361c0df91c2016b0615a8d86bae030
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-18 18:14:24 -0700

    vp-fe-fetch

--- frontend/src/page/view_profile.rs
+++ frontend/src/page/view_profile.rs
@@ -19,5 +19,25 @@ pub fn ViewProfile(cx: Scope) -> Element {
         .and_then(|id| UserId::from_str(id).ok())
         .unwrap_or_default();
 
+    use_effect(cx, (&user_id,), |(user_id,)| {
+        to_owned![api_client, post_manager, profile, toaster];
+        async move {
+            post_manager.write().clear();
+            use uchat_endpoint::user::endpoint::{ViewProfile, ViewProfileOk};
+            let request = ViewProfile { for_user: user_id };
+            let response = fetch_json!(<ViewProfileOk>, api_client, request);
+            match response {
+                Ok(res) => {
+                    profile.with_mut(|profile| *profile = Some(res.profile));
+                    post_manager.write().populate(res.posts.into_iter());
+                }
+                Err(e) => toaster.write().error(
+                    format!("Failed to retrieve posts: {e}"),
+                    chrono::Duration::seconds(3),
+                ),
+            }
+        }
+    });
+
     cx.render(rsx! {""})
 }
