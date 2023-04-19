f2a631cfb4cf1b53d5bc0a565799212792a51a89
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-19 16:52:58 -0700

    sb-buttons

--- frontend/src/elements/sidebar.rs
+++ frontend/src/elements/sidebar.rs
@@ -29,6 +29,8 @@ impl SidebarManager {
 
 pub fn Sidebar(cx: Scope) -> Element {
     let sidebar = use_sidebar(cx);
+    let router = use_router(cx);
+    let local_profile = use_local_profile(cx);
 
     let sidebar_width = if sidebar.read().is_open() {
         "w-[var(--sidebar-width)]"
@@ -55,7 +57,28 @@ pub fn Sidebar(cx: Scope) -> Element {
             class: "{sidebar_width} z-[100] fixed top-0 left-0 h-full
             overflow-x-hidden
             flex flex-col
-            navbar-bg-color transition-[width] duration-300"
+            navbar-bg-color transition-[width] duration-300",
+            a {
+                class: "sidebar-navlink border-t",
+                onclick: move |_| {
+                    sidebar.write().close();
+                    router.navigate_to(page::PROFILE_EDIT);
+                },
+                "Edit Profile"
+            }
+            a {
+                class: "sidebar-navlink",
+                onclick: move |_| {
+                    use chrono::Utc;
+                    use uchat_domain::ids::SessionId;
+                    crate::util::cookie::set_session("".to_string(), SessionId::new(), Utc::now());
+                    local_profile.write().user_id = None;
+                    local_profile.write().image = None;
+                    sidebar.write().close();
+                    router.navigate_to(page::ACCOUNT_LOGIN);
+                },
+                "Logout"
+            }
         }
     })
 }
