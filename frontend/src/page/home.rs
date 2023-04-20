1ffff77536b71099ac084f6baa557682766d3548
Jayson Lennon <jayson@jaysonlennon.dev>
2023-04-20 08:59:04 -0700

    empty-home

--- frontend/src/page/home.rs
+++ frontend/src/page/home.rs
@@ -31,7 +31,31 @@ pub fn Home(cx: Scope) -> Element {
         })
     };
 
-    let Posts = post_manager.read().all_to_public();
+    let Posts = {
+        let posts = post_manager.read().all_to_public();
+        if posts.is_empty() {
+            let TrendingLink = rsx! {
+                a {
+                    class: "link",
+                    onclick: move |_| {
+                        router.navigate_to(page::POSTS_TRENDING);
+                    },
+                    "trending"
+                },
+            };
+            rsx! {
+                div {
+                    class: "flex flex-col text-center justify-center
+                    h-[calc(100vh_-_var(--navbar-height)_-_var(--appbar-height))]",
+                    span {
+                        "Check out what's ", TrendingLink ", and follow some users to get started."
+                    }
+                }
+            }
+        } else {
+            rsx! { posts.into_iter() }
+        }
+    };
 
     cx.render(rsx! {
         Appbar {
@@ -59,6 +83,6 @@ pub fn Home(cx: Scope) -> Element {
 
         },
 
-        Posts.into_iter()
+        Posts
     })
 }
