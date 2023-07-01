83c620de7101e6e097a7026fc466d58ec2923888
Michael Robards <mikerobards@icloud.com>
2023-07-01 10:12:15 -0400

    add hello world

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,9 +1,11 @@
 #![allow(non_snake_case)]
 
-use dioxus::prelude::*;
+use dioxus::{html::h1, prelude::*};
 use fermi::use_init_atom_root;
 
 pub fn App(cx: Scope) -> Element {
     use_init_atom_root(cx);
-    None
+    cx.render(rsx! {
+        h1 { "Hello, World!" }
+    })
 }
