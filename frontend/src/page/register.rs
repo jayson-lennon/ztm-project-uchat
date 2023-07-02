927ba6f59a5f2e9a659dd4f7f0a7df833143ee2c
Michael Robards <mikerobards@icloud.com>
2023-07-02 09:42:21 -0400

    add page state

--- frontend/src/page/register.rs
+++ frontend/src/page/register.rs
@@ -2,7 +2,23 @@
 
 use dioxus::prelude::*;
 
+pub struct PageState {
+    username: UseState<String>,
+    password: UseState<String>,
+}
+
+impl PageState {
+    pub fn new(cx: Scope) -> Self {
+        Self {
+            username: use_state(cx, String::new).clone(),
+            password: use_state(cx, String::new).clone(),
+        }
+    }
+}
+
 pub fn Register(cx: Scope) -> Element {
+    let page_state = PageState::new(cx);
+    let page_state = use_ref(cx, || page_state);
     cx.render(rsx! {
         form {
             class: "flex flex-col gap-5",
