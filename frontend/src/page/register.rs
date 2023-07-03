691cb175a0114c5cfa4ded7e2e6c7ff5fe6831cb
Michael Robards <mikerobards@icloud.com>
2023-07-03 09:42:29 -0400

    add username and password inputs

--- frontend/src/page/register.rs
+++ frontend/src/page/register.rs
@@ -2,6 +2,8 @@
 
 use dioxus::prelude::*;
 
+use crate::prelude::*;
+
 pub struct PageState {
     username: UseState<String>,
     password: UseState<String>,
@@ -16,14 +18,84 @@ impl PageState {
     }
 }
 
+#[inline_props]
+pub fn UsernameInput<'a>(
+    cx: Scope<'a>,
+    state: UseState<String>,
+    oninput: EventHandler<'a, FormEvent>,
+) -> Element<'a> {
+    cx.render(rsx! {
+        div {
+            class: "flex flex-col",
+            label {
+                r#for: "username",
+                "Username",
+            },
+            input {
+                id: "username",
+                name: "username",
+                class: "input-field",
+                placeholder: "User Name",
+                value: "{state.current()}",
+                oninput: move |ev| oninput.call(ev),
+            }
+        }
+    })
+}
+
+#[inline_props]
+pub fn PasswordInput<'a>(
+    cx: Scope<'a>,
+    state: UseState<String>,
+    oninput: EventHandler<'a, FormEvent>,
+) -> Element<'a> {
+    cx.render(rsx! {
+        div {
+            class: "flex flex-col",
+            label {
+                r#for: "password",
+                "Password",
+            },
+            input {
+                id: "password",
+                r#type: "password",
+                name: "password",
+                class: "input-field",
+                placeholder: "Password",
+                value: "{state.current()}",
+                oninput: move |ev| oninput.call(ev),
+            }
+        }
+    })
+}
+
 pub fn Register(cx: Scope) -> Element {
     let page_state = PageState::new(cx);
     let page_state = use_ref(cx, || page_state);
+
+    let username_oninput = sync_handler!([page_state], move |ev: FormEvent| {
+        page_state.with_mut(|state| state.username.set(ev.value.clone()));
+    });
+
+    let password_oninput = sync_handler!([page_state], move |ev: FormEvent| {
+        page_state.with_mut(|state| state.password.set(ev.value.clone()));
+    });
+
     cx.render(rsx! {
         form {
             class: "flex flex-col gap-5",
             prevent_default: "onsubmit",
             onsubmit: move |_| {},
+
+            UsernameInput {
+                state: page_state.with(|state| state.username.clone()),
+                oninput: username_oninput,
+            },
+
+            PasswordInput {
+                state: page_state.with(|state| state.password.clone()),
+                oninput: password_oninput,
+            },
 
             button {
                 class: "btn",
